c0bb8193a5c011ec8af67d00f2a6eb0f1daf7694
Clicker <unkownmalesir@gmail.com>
2025-02-28 21:47:59 -0500

    Update games.js

--- games.js
+++ games.js
@@ -1,7 +1,8 @@
 const games = [
     { name: "Minecraft", url: "games/minecraft.html", img: "https://tse4.mm.bing.net/th?id=OIP.l6MZZynh7ZPVmvvTNNvAFAHaEK&pid=Api&P=0&h=180" },
     { name: "Pac-Man", url: "games/pacman.html", img: "https://tse1.mm.bing.net/th?id=OIP._Gw9uH3x7VwKX4nODkjlcQHaEK&pid=Api&P=0&h=180" },
-    { name: "Tetris", url: "games/tetris.html", img: "https://pkmanager.twingalaxies.com/assets/article/2019/06/06/tetris-celebrates-its-35th-anniversary-with-new-logo-events_feature.jpg" },
+    { name: "Tetris", url: "games/tetris.html", img: "https://pkmanager.twingalaxies.com/assets/article/2019/06/06/tetris-celebrates-its-35th-anniversary-with-new-logo-events_feature.jpg" 
+    { name: "BasketBallBros", url: "games/BasketBallBros.html", img: "https://tse2.mm.bing.net/th?id=OIP.PD4xDZFWt7dxpOCQDsmTdQHaEK&pid=Api&P=0&h=180" },
     // Add more games here
 ];
 
